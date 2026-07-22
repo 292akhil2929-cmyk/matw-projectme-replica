1e78fc36150561f850e4ed7f59f4002424ced1cd
292akhil2929-cmyk <79akhil105@gmail.com>
2026-07-22 10:31:49 +0400

    Add MATW dropdown navigation and route coverage

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { Footer, Header, InnerPage } from "../page";
+import { Footer, Header, InnerPage, MegaNav } from "../page";
 
 export default function CatchAllPage() {
   const path = usePathname() || "/top-10";
-  return <><Header /><InnerPage path={path} /><Footer /></>;
+  return <><Header /><MegaNav /><InnerPage path={path} /><Footer /></>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./overrides.css";
+import "./mega.css";
 
 export const metadata: Metadata = {
   title: "MATW Project | Muslim Charity",
