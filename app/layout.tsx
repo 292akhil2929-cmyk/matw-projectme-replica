96a7119c383361924948c323df47a91980d9a27b
292akhil2929-cmyk <79akhil105@gmail.com>
2026-07-22 12:33:45 +0400

    Replace legacy animation with interactive story chapters

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import "./original-project.css";
 import "./priority-pages.css";
 import "./calculator-embed.css";
 import "./interactive-priority.css";
-import "./ali-animation.css";
+import "./legacy-path.css";
 
 export const metadata: Metadata = {
   title: "MATW Project | Muslim Charity",
